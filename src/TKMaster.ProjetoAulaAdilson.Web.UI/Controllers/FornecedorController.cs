a8da11a5e3be40c2f86a08edcd13d54758a1e822
tkmaster1 <45917644+tkmaster1@users.noreply.github.com>
2022-02-07 09:54:58 -0300

    Implementaçao do layout AdminLTE

---
+++ src/TKMaster.ProjetoAulaAdilson.Web.UI/Controllers/FornecedorController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TKMaster.ProjetoAulaAdilson.Web.UI.Controllers
+{
+    public class FornecedorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
